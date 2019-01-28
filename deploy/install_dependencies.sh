f9b621e2f13c097a695d9106584a388443fcce71
pyaephyoeshein@gmail.com <pyaephyoeshein@gmail.com>
2019-01-28 22:49:25 +0630

    Install scripts changed

--- deploy/install_dependencies.sh
+++ deploy/install_dependencies.sh
@@ -2,11 +2,9 @@
 set -e
 # update instance
 yum -y update
-# install general libraries like Java or ImageMagick
-yum -y install default-jre ImageMagick
-# add nodejs to yum
-curl --silent --location https://rpm.nodesource.com/setup_4.x | bash -
-yum -y install nodejs #default-jre ImageMagick
-# install pm2 module globaly
-npm install -g pm2
-pm2 update
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
+
+. ~/.nvm/nvm.sh
+
+nvm install node
