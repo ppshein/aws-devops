43735ba02fe6ba55e3ac0ddb198bff60d9cff09c
pyaephyoeshein@gmail.com <pyaephyoeshein@gmail.com>
2019-01-28 16:13:05 +0630

    first commit

---
+++ deploy/install_dependencies.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# update yum just in case
+yum update -y
+
+# get node into yum
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+
+# install node and npm in one line
+nvm install node
+
+npm install
---
+++ deploy/start_server.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Configuration"
+
+export NODE_ENV=production
+
+BASE_DIR=/home/ec2-user/
+APPLICATION_DIRECTORY=$BASE_DIR/aws-devops
+
+echo "cd"
+
+cd "$APPLICATION_DIRECTORY"
+
+#sequelize db:create && sequelize db:migrate
+
+# actually start the server
+npm start
